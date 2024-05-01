d4441eab83fb8fbdebfe403d27e895d35de48993
Andrew Duffy <andreweduffy@gmail.com>
2024-05-01 13:30:57 -0400

    fix flake.nix to work on all platforms

--- flake.nix
+++ flake.nix
@@ -1,34 +1,36 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
     rust-overlay.url = "github:oxalica/rust-overlay";
   };
 
   outputs = {
     self,
     nixpkgs,
+    flake-utils,
     rust-overlay,
-  }: let
-    # TODO what's the correct incantation here?
-    system = "x86_64-linux";
+  }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+    	  inherit system;
+    	  overlays = [rust-overlay.overlays.default];
+    	};
 
-    pkgs = import nixpkgs {
-      inherit system;
-      overlays = [rust-overlay.overlays.default];
-    };
+    	toolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
 
-    toolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
-
-  in {
-    devShells.${system}.default = pkgs.mkShell {
-      packages = [
-        toolchain
-        pkgs.go_1_19
-        pkgs.just
-        pkgs.deno
-        pkgs.gofumpt
-      ];
-    };
-  };
+      in {
+        devShells.default = pkgs.mkShell {
+          packages = [
+            toolchain
+            pkgs.go_1_19
+            pkgs.just
+            pkgs.deno
+            pkgs.gofumpt
+          ];
+        };
+      }
+    );
 }
 
